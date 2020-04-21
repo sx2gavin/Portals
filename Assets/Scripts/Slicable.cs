a9bf296efc51f0d519e57fe0c26ab5b987001037
Gavin Luo <luoxiangyu528@gmail.com>
2020-04-20 22:08:34 -0400

    Add slice shader.

---
+++ Assets/Scripts/Slicable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slicable : MonoBehaviour
+{
+    public GameObject Slice;
+    private MeshRenderer mesh;
+    private Material material;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+        material = mesh.material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (material != null)
+        {
+            material.SetVector("sliceCentre", Slice.transform.position);
+            material.SetVector("sliceNormal", Slice.transform.forward);
+        }
+    }
+}
---
+++ Assets/Scripts/Slice.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class Slice : MonoBehaviour
+{
+    private MeshRenderer mesh;
+    private Material material;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+        material = mesh.material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (material != null)
+        {
+            material.SetVector("sliceCentre", transform.position);
+            material.SetVector("sliceNormal", transform.forward);
+        }
+    }
+}
