279e93bbd9bc9063b6fcc80a3ef6382b31e7bacc
sx2gavin <luoxiangyu528@gmail.com>
2020-06-12 12:47:23 -0400

    Add some modifications while writing the blog.

---
+++ Assets/Scripts/Slice.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Slice : MonoBehaviour
+{
+    [SerializeField] Sliceable sliceable;
+    [SerializeField] Text position;
+    [SerializeField] Text normal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sliceable.SlicePosition = transform.position;
+        sliceable.SliceNormal = transform.forward;
+        position.text = transform.position.ToString();
+        normal.text = transform.forward.ToString();
+    }
+}
